07ac054224d7a014611f7c76ddcfdbad8ab0b538
mrmops <kiriell.marshall@gmail.com>
2021-06-30 02:04:30 +0500

    Добавил заглушку списка, сделал базовые поля и скрытие большого контента

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -39,19 +40,31 @@ class MainPageState extends State<MainPage> {
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
-        title: Text('Side menu'),
+        title: Text('Habits'),
         actions: [
           IconButton(
             icon: const Icon(Icons.filter_alt_outlined),
-            tooltip: 'OpenFilters',
+            tooltip: 'Открыть фильтры!',
             onPressed: _showPersistantBottomSheetCallBack,
           ),
         ],
       ),
-      body: Center(child: Text('Ololo')),
+      body: ListView.builder(
+        itemBuilder: (context, position) {
+          return HabitItem();
+        },
+        itemCount: 200,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        tooltip: 'Добавить привычку!',
+      ),
     );
   }
-
   void showBottomSheet() {
     setState(() {
       _showPersistantBottomSheetCallBack = null;
@@ -64,11 +77,7 @@ class MainPageState extends State<MainPage> {
               height: 200.0,
               color: Colors.teal[100],
               child: Center(
-                child: Text(
-                  "Drag Downwards Or Back To Dismiss Sheet",
-                  style: TextStyle(fontSize: 18, color: Colors.black),
-                  textAlign: TextAlign.center,
-                ),
+                child: FilterFragment(),
               ),
             );
           },
@@ -84,13 +93,77 @@ class MainPageState extends State<MainPage> {
   }
 }
 
+class HabitItem extends StatefulWidget {
+  @override
+  State<HabitItem> createState() => HabitItemState();
+}
+
+class HabitItemState extends State<HabitItem> {
+  bool _hiddenState = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicHeight(
+      child: Column(
+        children: [
+          Container(
+            height: 64,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            'Name',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          Text(
+                            '30.02.2077 +5',
+                            style: TextStyle(fontSize: 14),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Text("Priority"),
+                  ),
+                  Container(
+                    child: IconButton(
+                      icon: const Icon(Icons.arrow_drop_down_sharp),
+                      onPressed: () { setState(() {
+                            _hiddenState = !_hiddenState;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          if(!_hiddenState) Text("Description")
+        ],
+      ),
+    );
+  }
+}
+
 class FilterFragment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.amber,
       child: Center(
-        child: Text('Фильтрация'),
+        child: Text(
+          'Фильтрация',
+          style: TextStyle(fontSize: 18, color: Colors.black),
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }

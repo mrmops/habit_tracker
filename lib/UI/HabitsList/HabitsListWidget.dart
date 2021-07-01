b2ff7b972f43d3b0fe1a78e8939ea45eee9bb47f
mrmops <kiriell.marshall@gmail.com>
2021-07-01 11:20:38 +0500

    Изменил верстку элемента привычки

--- lib/UI/HabitsList/HabitsListWidget.dart
+++ lib/UI/HabitsList/HabitsListWidget.dart
@@ -22,72 +22,117 @@ class HabitItemState extends State<HabitItem> {
 
   @override
   Widget build(BuildContext context) {
-    return IntrinsicHeight(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            Container(
-              height: 64,
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 8),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Text(
-                              'Name',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                            Text(
-                              '30.02.2077 +5',
-                              style: TextStyle(fontSize: 14),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      child: Text('Priority'),
-                    ),
-                    Container(
-                      child: IconButton(
-                        icon: _hiddenState
-                            ? const Icon(Icons.arrow_drop_down_sharp)
-                            : const Icon(Icons.arrow_drop_up_sharp),
-                        onPressed: () {
-                          setState(() {
-                            _hiddenState = !_hiddenState;
-                          });
-                        },
-                      ),
-                    ),
-                  ],
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('Name'),
+            subtitle: Text('30.02.2077 +5'),
+            trailing: Wrap(
+              direction: Axis.horizontal,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              children: [
+                Text('Normal'),
+                IconButton(
+                  icon: _hiddenState
+                      ? const Icon(Icons.arrow_drop_down_sharp)
+                      : const Icon(Icons.arrow_drop_up_sharp),
+                  onPressed: () {
+                    setState(() {
+                      _hiddenState = !_hiddenState;
+                    });
+                  },
                 ),
-              ),
+              ],
             ),
-            if (!_hiddenState) buildHiddenProperties(),
-          ],
-        ),
+          ),
+          if (!_hiddenState) buildHiddenProperties(),
+        ],
       ),
     );
   }
 
+
   Widget buildHiddenProperties() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [Text('Number of completed'), Text('still need')],
-        ),
-        Text('Description')
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [Text('Number of completed'), Text('still need')],
+          ),
+          Text('Description')
+        ],
+      ),
     );
   }
+
+  //TODO delete old version
+  // @override
+  // Widget build(BuildContext context) {
+  //   return IntrinsicHeight(
+  //     child: Padding(
+  //       padding: EdgeInsets.symmetric(horizontal: 16),
+  //       child: Column(
+  //         children: [
+  //           Container(
+  //             height: 64,
+  //             child: Padding(
+  //               padding: EdgeInsets.symmetric(vertical: 8),
+  //               child: Row(
+  //                 children: [
+  //                   Expanded(
+  //                     child: Column(
+  //                       crossAxisAlignment: CrossAxisAlignment.start,
+  //                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //                       children: <Widget>[
+  //                         Text(
+  //                           'Name',
+  //                           style: TextStyle(fontSize: 18),
+  //                         ),
+  //                         Text(
+  //                           '30.02.2077 +5',
+  //                           style: TextStyle(fontSize: 14),
+  //                         ),
+  //                       ],
+  //                     ),
+  //                   ),
+  //                   Text('Normal'),
+  //                   IconButton(
+  //                     icon: _hiddenState
+  //                         ? const Icon(Icons.arrow_drop_down_sharp)
+  //                         : const Icon(Icons.arrow_drop_up_sharp),
+  //                     onPressed: () {
+  //                       setState(() {
+  //                         _hiddenState = !_hiddenState;
+  //                       });
+  //                     },
+  //                   ),
+  //                 ],
+  //               ),
+  //             ),
+  //           ),
+  //           if (!_hiddenState) buildHiddenProperties(),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  //}
+  //
+  // Widget buildHiddenProperties() {
+  //   return Column(
+  //     mainAxisAlignment: MainAxisAlignment.start,
+  //     crossAxisAlignment: CrossAxisAlignment.start,
+  //     children: [
+  //       Row(
+  //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //         children: [Text('Number of completed'), Text('still need')],
+  //       ),
+  //       Text('Description')
+  //     ],
+  //   );
+  // }
 }
